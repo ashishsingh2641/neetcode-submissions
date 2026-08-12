fd2338fac92555fda8732913510ce1c6a018fef0
ashishsingh2641 <ashishsingh2641@gmail.com>
2026-08-13 00:43:34 +0530

    Add: python-dynamic-typing - submission-3

---
+++ Python For Beginners/python-dynamic-typing/submission-3.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 20.30
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "hello"
+print(type(variable))
+variable = [10,20,30,40,50]
+print(type(variable))
