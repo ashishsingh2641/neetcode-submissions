cafdd68e0967d98a3f7c957d3e6aea222269d461
ashishsingh2641 <ashishsingh2641@gmail.com>
2026-08-13 00:41:44 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+integer_type = 20
+float_type = 30.34
+bool_type = True
+string_type = "hello"
+list_type = [20, 30,40,50]
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
